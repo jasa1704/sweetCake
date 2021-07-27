e368fd950980c50d8129cfdd3f274dda62911d73
Jhonatan Sosapanta <jasosapanta@gmail.com>
2021-07-26 23:17:02 -0500

    Challenge resolved, menu e-commerce

---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './NavBar.scss';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import logo from '../../assets/img/logo-sweet-cake.png'
+
+export default function NavBar() {
+    return (
+        <Navbar collapseOnSelect expand="lg" variant="light" className="style-nav">
+            <Container>
+                <Navbar.Brand href="#home">
+                    <img
+                        src={logo}
+                        width="70"
+                        height="70"
+                        className="d-inline-block align-top"
+                        alt="logo dulce pastel"
+                    />
+                </Navbar.Brand>
+                <Navbar.Brand href="#home" className="title-brand">Dulce pastel</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+                    <Nav>
+                        <Nav.Link href="#home">Inicio</Nav.Link>
+                        <Nav.Link href="#product">Productos</Nav.Link>
+                        <Nav.Link href="#we">Nosotros</Nav.Link>
+                        <Nav.Link href="#contact">Contacto</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
